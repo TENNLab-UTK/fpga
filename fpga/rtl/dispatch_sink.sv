042ee5f9f63bd2298a2926e2146e5b5f45352365
Keegan Dent <keegandent@gmail.com>
2024-06-26 19:58:05 +0000

    Update file dispatch_sink.sv

--- fpga/rtl/dispatch_sink.sv
+++ fpga/rtl/dispatch_sink.sv
@@ -35,46 +35,52 @@ module network_sink (
     // 1. the indices of the net outputs that fired in descending order
     // 2. the number of net outputs that fired
     // of these, 2 is always populated, and the queue is sent out back-to-front
-    logic [$clog2(NET_NUM_OUT + 2)-1:0] snk_counter;
-    logic [$clog2(NET_NUM_OUT + 2)-1:0] next_snk_counter;
     logic [SNK_WIDTH-1:0] snk_queue [0:(NET_NUM_OUT)];   // not -1 because of num_out
-    logic [SNK_WIDTH-1:0] next_snk_queue [0:(NET_NUM_OUT)];
 
-    // FIXME: This logic is not working.
-    // There might not be a way to accomplish single-cycle queue population in synthesizeable code.
-    always_comb begin : calc_next_snk
-        int i = 0;
-        // loop in reverse so we can count down queue positions using snk_counter
-        for (int j = NET_NUM_OUT - 1; j >= 0; j--) begin
-            if (net_out[j]) begin
-                next_snk_queue[i] = j;
-                i++;
+    logic [$clog2(NET_NUM_OUT + 2)-1:0] snk_counter, pop_counter;
+    assign snk_valid = (pop_counter == 0) && (snk_counter > 0);
+    assign net_ready = (pop_counter == 0) && (snk_counter == 0);
+
+    logic pop_fire = net_out[NET_NUM_OUT + 1 - pop_counter];
+
+    always_ff @(posedge clk or negedge arstn) begin: set_pop_counter
+        if (arstn == 0) begin
+            pop_counter <= 0;
+        end else begin
+            if (net_valid && net_ready) begin
+                pop_counter <= NET_NUM_OUT + 1;
+            end else if (pop_counter > 0) begin
+                pop_counter <= pop_counter - 1;
             end
         end
-        next_snk_queue[i] = i;
-        next_snk_counter = i + 1;
     end
 
     always_ff @(posedge clk or negedge arstn) begin: set_snk_queue
         if (arstn == 0) begin
             foreach (snk_queue[i])
                 snk_queue[i] <= 0;
-        end else if (net_valid && net_ready) begin
-            snk_queue <= next_snk_queue;
+        end else begin
+            if (net_valid && net_ready) begin
+                foreach (snk_queue[i])
+                    snk_queue[i] <= 0;
+            end else if (pop_counter > 0) begin
+                if (pop_counter == 1) begin
+                    snk_queue[snk_counter] <= snk_counter;
+                end else if (pop_fire) begin
+                    snk_queue[snk_counter] <= NET_NUM_OUT + 1 - pop_counter;
+                end
+            end
         end
     end
 
-    assign snk_valid = (snk_counter > 0);
-    assign net_ready = (snk_counter == 0);
-
-    always_ff @(posedge clk or negedge arstn) begin : set_snk_counter
+    always_ff @(posedge clk or negedge arstn) begin: set_snk_counter
         if (arstn == 0) begin
             snk_counter <= 0;
         end else begin
-            if (snk_valid && snk_ready) begin
+            if (pop_fire && (pop_counter > 1)) begin
+                snk_counter <= snk_counter + 1;
+            end esle if (snk_valid && snk_ready) begin
                 snk_counter <= snk_counter - 1;
-            end else if (net_valid && net_ready) begin
-                snk_counter <= next_snk_counter;
             end
         end
     end
