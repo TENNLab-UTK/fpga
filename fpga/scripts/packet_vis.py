f7727a3c546c7cdfd67e6bfa4aa72520e3b3add1
Keegan Dent <keegandent@gmail.com>
2024-07-22 18:21:21 -0500

    way better UI for packet vis but needs linting and formatting

--- fpga/scripts/packet_vis.py
+++ fpga/scripts/packet_vis.py
@@ -5,6 +5,7 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 import base64
 
+import dash_bootstrap_components as dbc
 from dash import Dash, Input, Output, State, callback, dcc, html
 from neuro import Network
 
@@ -12,102 +13,166 @@ import fpga._processor
 from fpga._processor import _IoType
 from fpga.network import charge_width
 
-app = Dash()
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-app.layout = [
-    html.H1("Packet Visualization"),
-    html.Div(
-        [
-            "Network File",
-            dcc.Upload(
-                children=html.Div(
-                    [
-                        "Drag and Drop or ",
-                        html.A("Select File"),
-                    ]
+app.layout = dbc.Container(
+    [
+        dbc.Row(dbc.Col(html.H1("Packet Visualization"))),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dcc.Upload(
+                        children=dbc.InputGroup(
+                            [
+                                dbc.InputGroupText("Drag and Drop or "),
+                                dbc.Button("Select Network File"),
+                            ]
+                        ),
+                        id="network_file",
+                        multiple=False,
+                        # accept="*.txt,*.json",
+                    ),
+                    width="auto",
                 ),
-                id="network_file",
-                multiple=False,
-                # accept="*.txt,*.json",
-            ),
-        ]
-    ),
-    html.Div(
-        [
-            "Parameters from:",
-            dcc.RadioItems(
-                id="param_select",
-                options=[
-                    {"label": "File", "value": "File", "disabled": True},
-                    {"label": "Manual", "value": "Manual"},
+                dbc.Col(dbc.Label("Parameters from:"), width="auto"),
+                dbc.Col(
+                    dbc.RadioItems(
+                        id="param_select",
+                        options=[
+                            {"label": "File", "value": "File", "disabled": True},
+                            {"label": "Manual", "value": "Manual"},
+                        ],
+                        value="Manual",
+                        inline=True,
+                    ),
+                    width="auto",
+                ),
+            ],
+            align="center",
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        html.H2("Processor Parameters"),
+                        dbc.Row(
+                            [
+                                dbc.Col(dbc.Label("Charge Width")),
+                                dbc.Col(
+                                    [
+                                        # Cannot use fewer than 2 bits for signed int
+                                        dbc.Input(
+                                            id="charge_width",
+                                            type="number",
+                                            value=8,
+                                            min=2,
+                                        ),
+                                    ]
+                                ),
+                            ],
+                            align="center",
+                        ),
+                    ],
+                    width="auto",
+                ),
+                dbc.Col(
+                    [
+                        html.H2("Network Parameters"),
+                        dbc.Row(
+                            [
+                                dbc.Col(dbc.Label("Number of Input Neurons")),
+                                dbc.Col(
+                                    [
+                                        dbc.Input(
+                                            id="num_inp",
+                                            type="number",
+                                            value=1,
+                                            min=1,
+                                        ),
+                                    ]
+                                ),
+                            ],
+                            align="center",
+                        ),
+                        dbc.Row(
+                            [
+                                dbc.Col(dbc.Label("Number of Output Neurons")),
+                                dbc.Col(
+                                    [
+                                        dbc.Input(
+                                            id="num_out",
+                                            type="number",
+                                            value=1,
+                                            min=1,
+                                        ),
+                                    ]
+                                ),
+                            ],
+                            align="center",
+                        ),
+                    ],
+                    width="auto",
+                )
+            ],
+        ),
+        dbc.Row(
+            dbc.Col(
+                [
+                    html.H2("Source"),
+                    dbc.Row(
+                        [
+                            dbc.Col(dbc.Label("Source Type")),
+                            dbc.Col(
+                                dbc.RadioItems(
+                                    id="source_type",
+                                    options=[io_name.capitalize() for io_name in _IoType._member_names_],
+                                    value=_IoType.DISPATCH.name.capitalize(),
+                                ),
+                                width="auto",
+                            ),
+                        ]
+                    ),
+                    dbc.Row(
+                        [
+                            dbc.Col(dbc.Label("Opcode")),
+                            dbc.Col(dbc.Select(id="opcode", )),
+                        ]
+                    ),
                 ],
-                value="Manual",
-            ),
-        ]
-    ),
-    html.H2("Processor Parameters"),
-    html.Div(
-        [
-            "Charge Width",
-            # Cannot use fewer than 2 bits to represent a signed value
-            dcc.Input(id="charge_width", type="number", value=8, min=2),
-        ]
-    ),
-    html.H2("Network Parameters"),
-    html.Div(
-        [
-            "Number of Input Neurons",
-            dcc.Input(id="num_inp", type="number", value=1, min=1),
-        ]
-    ),
-    html.Div(
-        [
-            "Number of Output Neurons",
-            dcc.Input(id="num_out", type="number", value=1),
-        ]
-    ),
-    html.Hr(),
-    html.H2("Source"),
-    html.Div(
-        [
-            "Source Type",
-            dcc.Dropdown(
-                id="source_type",
-                options=[io_name.capitalize() for io_name in _IoType._member_names_],
-                value=_IoType.DISPATCH.name.capitalize(),
-                clearable=False,
-            ),
-        ]
-    ),
-    html.Div(
-        [
-            "Opcode",
-            dcc.Dropdown(id="opcode"),
-        ]
-    ),
-    html.Hr(),
-    html.H2(children="Sink"),
-    html.Div(
-        [
-            "Sink Type",
-            dcc.Dropdown(
-                id="sink_type",
-                options=[io_name.capitalize() for io_name in _IoType._member_names_],
-                value=_IoType.DISPATCH.name.capitalize(),
-                clearable=False,
-            ),
-        ]
-    ),
-]
+                width="auto",
+            )
+        ),
+        dbc.Row(
+            dbc.Col(
+                [
+                    html.H2("Sink"),
+                    dbc.Row(
+                        [
+                            dbc.Col(dbc.Label("Sink Type")),
+                            dbc.Col(
+                                dbc.RadioItems(
+                                        id="sink_type",
+                                        options=[io_name.capitalize() for io_name in _IoType._member_names_],
+                                        value=_IoType.DISPATCH.name.capitalize(),
+                                )
+                            ),
+                        ]
+                    ),
+                ],
+                width="auto",
+            )
+        ),
+    ]
+)
 
 
 @callback(
     Output("charge_width", "value"),
     Output("num_inp", "value"),
     Output("num_out", "value"),
-    Output("charge_width", "readOnly"),
-    Output("num_inp", "readOnly"),
-    Output("num_out", "readOnly"),
+    Output("charge_width", "readonly"),
+    Output("num_inp", "readonly"),
+    Output("num_out", "readonly"),
     Input("param_select", "value"),
     State("network_file", "contents"),
     State("charge_width", "value"),
@@ -145,12 +210,13 @@ def enable_param_select(contents, options):
 
 @callback(
     Output("opcode", "options"),
+    Output("opcode", "value"),
     Input("source_type", "value"),
 )
 def update_opcode_options(source_type_str):
     # HACK: this is probably asking for trouble
     opc_type = getattr(fpga._processor, "_" + source_type_str + "Opcode")
-    return opc_type._member_names_
+    return opc_type._member_names_, None
 
 
 if __name__ == "__main__":
