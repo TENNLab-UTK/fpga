9edadb1d3e4df2ae9c078396590f475bd1448bc6
Bryson Gullett <brysongullett@gmail.com>
2024-09-02 13:42:56 -0400

    DMA vivado project generation tmp script working

--- fpga/scripts/tmp_tcl_test.py
+++ fpga/scripts/tmp_tcl_test.py
@@ -1,10 +1,10 @@
 import subprocess
 
-with open(".tmp.tcl", 'w') as tcl_f:
-    tcl_f.write('''set argv [list "--inp_pkt_width_bits" "8" "--out_pkt_width_bits" "8"]\nset argc [llength $argv]\nset argv0 [file join [file dirname [info script]] pynq_dma.tcl]\nsource $argv0\n''')
+with open("tmp_tcl_test/.tmp.tcl", 'w') as tcl_f:
+    tcl_f.write('''set argv [list "--inp_pkt_width_bits" "8" "--out_pkt_width_bits" "8"]\nset argc [llength $argv]\nset argv0 [file join [file dirname [info script]] ../pynq_dma.tcl]\nsource $argv0\n''')
 
 bash_cmd = "vivado -mode batch -source .tmp.tcl"
 
-result = subprocess.run(bash_cmd.split(' '))
+result = subprocess.run(bash_cmd.split(' '), cwd="tmp_tcl_test")
 
 print(result.returncode)
