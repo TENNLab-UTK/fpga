c46ca48e63b19e8c544c08b0c787fc546619abc9
Bryson Gullett <brysongullett@gmail.com>
2024-09-12 00:12:58 -0400

    Add time testing to pynq dma outer test python

--- fpga/scripts/testing/pynq_dma_outer_test.py
+++ fpga/scripts/testing/pynq_dma_outer_test.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 import pathlib as pl
 from periphery import Serial
@@ -10,7 +11,7 @@ TEST_DIR_REL_PATH_BASE = "test_"
 TEMPLATE_NET_REL_PATH = "empty_net_risp.json"
 SERIAL_TERMINAL_BAUDRATE = 115200
 
-def test_pynq_dma(test_id: int, num_neurons: int, num_synapses: int, num_inputs: int, num_outputs: int, num_sim_times: int, sim_time: int, zynq_host: str, zynq_serial_device: str, zynq_password: str) -> None:
+def test_pynq_dma(test_id: int, num_neurons: int, num_synapses: int, num_inputs: int, num_outputs: int, num_sim_times: int, sim_time: int, zynq_host: str, zynq_serial_device: str, zynq_password: str) -> float:
 
     # Find path to this python file
     this_script_fpath = pl.Path(__file__).resolve()
@@ -26,10 +27,10 @@ def test_pynq_dma(test_id: int, num_neurons: int, num_synapses: int, num_inputs:
         test_dir_fpath.mkdir(parents=True, exist_ok=True)
 
     # Generate random network that is written out to a file
-    # template_net_json_fpath = pl.Path(str(this_script_fpath.parent) + "/" + TEMPLATE_NET_REL_PATH)
-    # rand_net_json_fpath = pl.Path(str(test_dir_fpath) + "/net.json")
-    # generate_network(str(template_net_json_fpath), str(framework_fpath), num_neurons, num_synapses, test_id, num_inputs, num_outputs, str(rand_net_json_fpath))
-    rand_net_json_fpath = pl.Path("/home/bryson/Documents/TENNLab/framework/fpga/networks/xor_noleak.txt")
+    template_net_json_fpath = pl.Path(str(this_script_fpath.parent) + "/" + TEMPLATE_NET_REL_PATH)
+    rand_net_json_fpath = pl.Path(str(test_dir_fpath) + "/net.json")
+    generate_network(str(template_net_json_fpath), str(framework_fpath), num_neurons, num_synapses, test_id, num_inputs, num_outputs, str(rand_net_json_fpath))
+    # rand_net_json_fpath = pl.Path("/home/bryson/Documents/TENNLab/framework/fpga/networks/xor_noleak.txt")
 
     # Generate random network input in the form of processor tool commands that are written to a file
     proc_tool_commands_fpath = pl.Path(str(test_dir_fpath) + "/proc_tool_commands.txt")
@@ -37,8 +38,7 @@ def test_pynq_dma(test_id: int, num_neurons: int, num_synapses: int, num_inputs:
     gen_inputs(str(rand_net_json_fpath), str(framework_fpath), str(proc_tool_commands_fpath), num_sim_times, sim_time, test_id, str(proc_tool_output_fires_fpath))
 
     # Generate network SystemVerilog and then bitstream for random network with PYNQ-Z1+petalinux target
-    zynq_dma_net_fpath = pl.Path(str(test_dir_fpath) + "/net_zynq_dma.json")
-    process_network(str(rand_net_json_fpath), str(zynq_dma_net_fpath), str(test_dir_fpath))
+    process_network(str(rand_net_json_fpath), str(test_dir_fpath))
 
     # Edit processor tool commands file to have the correct network file path
     proc_tool_commands_lines = []
@@ -49,7 +49,7 @@ def test_pynq_dma(test_id: int, num_neurons: int, num_synapses: int, num_inputs:
             if "ml" not in line.lower():
                 f.write(line)
             else:
-                f.write("ML /home/petalinux/zynq_framework/cpp-apps/networks/" + str(TEST_DIR_REL_PATH_BASE) + str(test_id) + "_" + zynq_dma_net_fpath.name + "\n")
+                f.write("ML /home/petalinux/zynq_framework/cpp-apps/networks/" + str(TEST_DIR_REL_PATH_BASE) + str(test_id) + "_" + rand_net_json_fpath.name + "\n")
 
     # Generate .bin file from bitstream
     bash_cmd = '''echo all:\n{\n    ''' + str(test_dir_fpath) + '''/pynq_dma.runs/impl_1/pynq_dma_wrapper.bit\n}\n'''
@@ -58,7 +58,7 @@ def test_pynq_dma(test_id: int, num_neurons: int, num_synapses: int, num_inputs:
     bash_cmd = "bootgen -image " + str(test_dir_fpath) + "/Full_Bitstream.bif -arch zynq -process_bitstream bin -w"
     subprocess.run(bash_cmd.split(' '))
 
-    # Copy pynq_dma network .bin file, network file, zynq_dma network file, processor tool inputs, and simulated processor tool outputs to zynq over network connection via scp
+    # Copy pynq_dma network .bin file, network file, processor tool inputs, and simulated processor tool outputs to zynq over network connection via scp
     # bash_cmd = "mkdir " + str(this_script_fpath.parent) + "/zynq_framework; git -C " + str(framework_fpath) + "archive HEAD | tar -x -C " + str(this_script_fpath.parent) + "/zynq_framework"
     # subprocess.run(bash_cmd.split(' '))
     # bash_cmd = "sshpass -p " + str(zynq_password) + " scp -r " + str(this_script_fpath.parent) + "/zynq_framework petalinux@" + str(zynq_host) + ":/home/petalinux/"
@@ -67,8 +67,6 @@ def test_pynq_dma(test_id: int, num_neurons: int, num_synapses: int, num_inputs:
     print(bash_cmd)
     subprocess.run(bash_cmd.split(' '))
     bash_cmd = "sshpass -p " + str(zynq_password) + " scp " + str(rand_net_json_fpath) + " petalinux@" + str(zynq_host) + ":/home/petalinux/zynq_framework/cpp-apps/networks/" + str(TEST_DIR_REL_PATH_BASE) + str(test_id) + "_" + rand_net_json_fpath.name
-    subprocess.run(bash_cmd.split(' '))
-    bash_cmd = "sshpass -p " + str(zynq_password) + " scp " + str(zynq_dma_net_fpath) + " petalinux@" + str(zynq_host) + ":/home/petalinux/zynq_framework/cpp-apps/networks/" + str(TEST_DIR_REL_PATH_BASE) + str(test_id) + "_" + zynq_dma_net_fpath.name
     subprocess.run(bash_cmd.split(' '))
     bash_cmd = "sshpass -p " + str(zynq_password) + " scp " + str(proc_tool_commands_fpath) + " petalinux@" + str(zynq_host) + ":/home/petalinux/"
     subprocess.run(bash_cmd.split(' '))
@@ -93,6 +91,20 @@ def test_pynq_dma(test_id: int, num_neurons: int, num_synapses: int, num_inputs:
             print(new_output)
             serial_output += new_output
 
+    ret = -1.0
+    if "are identical" in serial_output:
+        test_time_match = re.search("real\s*(\d*)h?(\d+)m(\d+\.\d+)s", serial_output)
+        if test_time_match:
+            test_times = test_time_match.groups()
+            if len(test_times) >= 3:
+                hours = 0
+                if test_times[0] != "":
+                    hours = float(test_times[0])
+                ret = hours*3600.0 + float(test_times[1])*60.0 + float(test_times[2])
+    
+    return ret
+
 
 if __name__ == '__main__':
-    test_pynq_dma(0, 100, 400, 40, 2, 2, 50, "10.42.0.128", "/dev/ttyUSB1", "tennlab")
+    res = test_pynq_dma(0, 10, 12, 4, 4, 2, 30, "10.42.0.128", "/dev/ttyUSB1", "tennlab")
+    print(res)
